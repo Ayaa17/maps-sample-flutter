313cd84b95e8d6fa7037e73e1cbe225d5050a71e
Aya <[kent831217@gmail.com]>
2024-12-11 21:07:13 +0800

    feat: add maps sample

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,15 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
+GMSServices.provideAPIKey("MAPS_API_KEY")
+
+ if let apiKey = loadAPIKeyFromEnv() {
+      GMSServices.provideAPIKey(apiKey)
+    } else {
+      // 如果没有加载到密钥，则使用备用密钥
+      GMSServices.provideAPIKey("your_fallback_api_key")
+    }
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
